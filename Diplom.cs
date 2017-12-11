3456555b973ec4504f85a0b23d7444533e3e8cf4
bogdan2060 <34454014+bogdan2060@users.noreply.github.com>
2017-12-11 19:38:49 +0200

    Add files via upload

---
+++ Diplom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Diplom
+    {
+        public int Kod_Manager;
+        public string PIB_Managera;
+        public string Nomer_Telefonu;
+        public double Adresa;
+        
+
+    }
+}
